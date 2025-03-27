9808febfac88a1c2e098d87dc7ef917498912dda
Jason Freeman <jason@blazanor.com>
2025-03-27 09:14:36 +0200

    Fix waybill and tracking links on order (#11)

--- wc-postnet-delivery.php
+++ wc-postnet-delivery.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * Plugin Name: Delivery Options For PostNet
  * Plugin URI: https://github.com/freeman-tech-systems/woocommerce-postnet-delivery
  * Description: Adds PostNet delivery options to WooCommerce checkout.
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: Freeman Tech Systems
  * Author URI: https://github.com/freeman-tech-systems
  * License: GPL2
@@ -247,7 +247,7 @@ function wc_postnet_delivery_enqueue_scripts($hook) {
   // Check if we are on the settings page of our plugin
   if ($hook == 'woocommerce_page_wc_postnet_delivery') {
     // Enqueue our script
-    wp_enqueue_script('wc-postnet-delivery-options-js', plugin_dir_url(__FILE__) . 'js/wc-postnet-delivery-options.js', array('jquery'), '1.0.4', true);
+    wp_enqueue_script('wc-postnet-delivery-options-js', plugin_dir_url(__FILE__) . 'js/wc-postnet-delivery-options.js', array('jquery'), '1.0.5', true);
     
     // Enqueue SweetAlert for nice alerts
     wp_enqueue_script('sweetalert2', 'https://cdn.jsdelivr.net/npm/sweetalert2@11', array(), '11.0', true);
@@ -782,8 +782,7 @@ function wc_postnet_delivery_checkout_field_update_order_meta($order_id) {
 }
 
 function wc_postnet_delivery_checkout_field_display_admin_order_meta($order) {
-  $store = json_decode(get_post_meta($order->get_id(), 'Destination Store', true));
-  echo '<p><strong>' . esc_html__('Destination Store', 'delivery-options-postnet-woocommerce') . ':</strong> ' . esc_html($store[1]) . '</p>';
+  wc_postnet_delivery_order_received_page($order);
 }
 
 function wc_postnet_delivery_validations() {
